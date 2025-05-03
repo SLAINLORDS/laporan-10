f5a64d7e4e41546e9682e7bcd97b89f629c43a75
SLAINLORDS <giovanno.westin24@gmail.com>
2025-05-03 18:47:18 +0700

    source

--- banding.py
+++ banding.py
@@ -1,9 +1,19 @@
-handle1=open("teks1.txt")
-handle2=open("teks2.txt")
-for line in handle1:
-    a=line
-    for baris in handle2:
-        b=baris
-    if a!=b:
-        print (F"{a} berbeda dengan {b}")
-        print()
+def banding(file1,file2):
+    handle1 = open(file1)
+    handle2 = open(file2)
+
+    baris = 1
+
+    for line1, line2 in zip(handle1, handle2):
+        if line1 != line2:
+            print(f"Perbedaan pada baris {baris}:")
+            print(f"File1: {line1.strip()}")
+            print(f"File2: {line2.strip()}")
+
+        baris += 1
+
+    handle1.close()
+    handle2.close()
+file1=("teks1.txt")
+file2=("teks2.txt")
+banding(file1,file2)
